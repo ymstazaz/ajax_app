a34a72c5a765cd4a1fe25b60bc122999fa880239
ymstazaz <r37a45d@gmail.com>
2024-10-21 15:51:31 +0900

    リクエストを処理する
    JSONへの指定などなど

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content])
+    # もともとあったPost以下をJsonで使うためにpost変数に格納しておく
+    render json:{ post: post }
+    # redirect_to action: :index　リダイレクトで更新されたインデックスにもっていっていたのをジェイソンに変更
+    # リダイレクトはURLを指定　レンダーはURLは変更せず
   end
 end
