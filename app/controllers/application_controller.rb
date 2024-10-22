f44491d608856b00f5851e29bea49ccd08e484f9
ymstazaz <r37a45d@gmail.com>
2024-10-22 17:44:38 +0900

    Basic認証の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  # 公開したウェブアプリのリンクにユーザー登録してる人しか見れないように設定↓
+  before_action :basic_auth
+
+# ↓定義してビフォアアクションで呼び出し.
+# 試す際にはレイルズSで更新
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更ら
+    end
+    # ユーザーネームとかを引っ張るために、ターミナルで（vim ~/.zshrc）してiでインサートモードに移行
+  end
 end
--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,8 +6,8 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:migrate
+bundle exec rake db:migrate
 # 終わったらデータベースヤムにもMySQLだけじゃなくてこっちも使うよっていう設定をする
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 # デプロイ時に自動でデータベースをからにするやつ。これを入れるのにあたって、 bundle exec rake db:migrateをコメントに移行
 # テキスト8311
